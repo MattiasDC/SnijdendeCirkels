df852cf19a7029468bb80e4240eb88a93ff66caf
MattiasDC <mattias.decharleroy@gmail.com>
2014-10-24 19:01:27 +0200

    ssh

--- src/Algoritme3.java
+++ src/Algoritme3.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.TreeSet;
 
 
-public class Algoritme3 extends Algoritme{
+public class Algoritme3 extends Algoritme {
 
 	public Algoritme3(ArrayList<Cirkel> cirkels, Main main) {
 		super(main);
@@ -16,51 +15,44 @@ public class Algoritme3 extends Algoritme{
 		Cirkel.count = 0;
 		ArrayList<Event> evenementen = maakEvenementen(cirkels);
 
-		TreeSet<Cirkel> queue = new TreeSet<Cirkel>(new Comparator<Cirkel>() {
+		TreeSet<Cirkel> queue = new TreeSet<Cirkel>((c1, c2) -> Double.compare(c1.getMiddelpunt()
+				.getY(), c2.getMiddelpunt().getY()));
 
-			@Override
-			public int compare(Cirkel c1, Cirkel c2) {
-				return Double.compare(c1.getMiddelpunt().getY(), c2.getMiddelpunt().getY());
-			}
-		});
-
-		TreeSet<Cirkel> stralen = new TreeSet<Cirkel>(new Comparator<Cirkel>() {
-
-			@Override
-			public int compare(Cirkel c1, Cirkel c2) {
-				return -Double.compare(c1.getStraal(), c2.getStraal());
-			}
-		});
+		TreeSet<Cirkel> stralen = new TreeSet<Cirkel>((c1, c2) -> -Double.compare(c1.getStraal(),
+				c2.getStraal()));
 
 		double maxStraal = 0;
-		for (Event evenement : evenementen){
-			if (evenement.links){
-				for (Cirkel cirkel : queue.subSet(new Cirkel(new Punt(evenement.x,evenement.cirkel.getTop()-maxStraal),0D),new Cirkel(new Punt(evenement.x,evenement.cirkel.getBottom()+maxStraal),0D))){
-					if(cirkel.getBottom() >= evenement.cirkel.getTop() && cirkel.getTop() <= evenement.cirkel.getBottom())
-						addSnijpunten(evenement.cirkel,cirkel);
+		for (Event evenement : evenementen) {
+			if (evenement.links) {
+				for (Cirkel cirkel : queue
+						.subSet(new Cirkel(new Punt(evenement.x, evenement.cirkel.getTop()
+								- maxStraal), 0D), new Cirkel(new Punt(evenement.x,
+								evenement.cirkel.getBottom() + maxStraal), 0D))) {
+					if (cirkel.getBottom() >= evenement.cirkel.getTop()
+							&& cirkel.getTop() <= evenement.cirkel.getBottom())
+						addSnijpunten(evenement.cirkel, cirkel);
 				}
 
-				if(evenement.cirkel.getStraal() > maxStraal)
+				if (evenement.cirkel.getStraal() > maxStraal)
 					maxStraal = evenement.cirkel.getStraal();
-				
+
 				stralen.add(evenement.cirkel);
 				queue.add(evenement.cirkel);
 			}
-			else{
+			else {
 				stralen.remove(evenement.cirkel);
 
-				if(evenement.cirkel.getStraal() == maxStraal){
+				if (evenement.cirkel.getStraal() == maxStraal) {
 					maxStraal = stralen.isEmpty() ? 0d : stralen.first().getStraal();
 				}
 
 				queue.remove(evenement.cirkel);
 			}
 		}
-		setRunningTime((int) ((System.nanoTime()-startTime)/1000000));
+		setRunningTime((int) ((System.nanoTime() - startTime) / 1000000));
 
-		if(Main.DEBUG){
+		if (Main.DEBUG) {
 			printInfo(3);
 		}
 	}
-
 }
